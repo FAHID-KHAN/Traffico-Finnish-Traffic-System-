ceff444a375f9b3f169163e73b377511018e9b0d
abir0 <abir.spsc@gmail.com>
2022-10-24 23:26:44 +0600

    Add pages, focus buttons and combo box items

--- app.py
+++ app.py
@@ -12,15 +12,44 @@ class MainWindow(QMainWindow):
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
 
+        # Add items to combo boxes
+        self.ui.locCombo.addItems([
+                                "-Select-",
+                                "All",
+                                "Municipalities",
+                                "State-roads",
+                                "Autori-oulu",
+                                "Autori-kuopio",
+                                "Paikannin-kuopio",
+        ])
+        self.ui.msgCombo.addItems([
+                                "-Select-",
+                                "All",
+                                "Municipalities",
+                                "State-roads",
+                                "Autori-oulu",
+                                "Autori-kuopio",
+                                "Paikannin-kuopio",
+        ])
+
+        # Pages
+        self.click_pages()
+
         # min window size
         # self.setMinimumSize(800, 640)
 
         # show window
         self.show()
 
+    def click_pages(self):
+        self.ui.mainButton.clicked.connect(lambda: self.ui.stackedWidget.setCurrentWidget(self.ui.mainPage))
+
+        self.ui.condButton.clicked.connect(lambda: self.ui.stackedWidget.setCurrentWidget(self.ui.condPage))
+
+        self.ui.msgButton.clicked.connect(lambda: self.ui.stackedWidget.setCurrentWidget(self.ui.msgPage))
+
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
     window = MainWindow()
-    window.show()
     sys.exit(app.exec_())
